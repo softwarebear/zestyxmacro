94832828ef1ccbd552fea307dec9fffb83daf53b
Richard Chandler <richard.chandler@1e.com>
2018-08-23 10:02:22 +0100

    example of the zesty x macro

---
+++ zestyMacro.cpp
@@ -0,0 +1,26 @@
+#define EVENT_CODE_GENERATOR(SELECTOR) \
+    SELECTOR(LeftMouseDown, LBUTTONDOWN, 0x0201) \
+    SELECTOR(LeftMouseUp, LBUTTONUP, 0x0202) \
+    SELECTOR(LeftMouseDoubleClick, LBUTTONDBLCLK, 0x0203)
+
+#define EVENT_ENUMS(function, message, number) WM_##message=number, 
+
+typedef enum 
+{ 
+    EVENT_CODE_GENERATOR( EVENT_ENUMS ) 
+    MaxEventNumber 
+} WindowsEvent;
+
+#define EVENT_OBSERVERS(function, message, number) void On##function(WindowsEvent windowsEvent)\
+{\
+    if (windowsEvent != WM_##message)\
+    { throw "Wrong Event Received"; } \
+}
+
+EVENT_CODE_GENERATOR( EVENT_OBSERVERS )
+
+int main(int, char**)
+{
+    WindowsEvent windowsEvent = WindowsEvent::WM_LBUTTONUP;
+    OnLeftMouseUp(windowsEvent);
+}
